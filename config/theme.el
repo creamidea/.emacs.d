40f3cfc30fac274a72d7b91c261935907e1666b4
creamidea <creamidea@gmail.com>
2016-04-06 12:13:16 +0800

    add is-graphic-p

--- config/theme.el
+++ config/theme.el
@@ -26,15 +26,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  ;; :font Menlo Monaco Fira-Mono Source-Code-Pro DejaVu-Sans-Mono
- '(default ((t (:family "DejaVu Sans Mono" :weight Regular :height 130 :width normal)))))
+ ;; :weight Regular bold normal
+ '(default ((t (:family "DejaVu Sans Mono" :weight bold :height 120 :width normal)))))
 ;; '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight Regular :height 160 :size 16 :width normal)))))
 
 ;; Setting Chinese Font
-(dolist (charset '(kana han symbol cjk-misc bopomofo))
-  (set-fontset-font (frame-parameter nil 'font)
-                    charset
-                    (font-spec :family "PingFang SC" :size 14)))
-;; (font-spec :family "Microsoft Yahei" :size 14 :height 156)))
+(if (display-graphic-p)
+    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+      (set-fontset-font (frame-parameter nil 'font)
+                        charset
+                        (font-spec :family "PingFang SC" :size 14)))
+  ;; (font-spec :family "Microsoft Yahei" :size 14 :height 156)))
+  )
 
 ;; set transparency
 ;; (set-frame-parameter (selected-frame) 'alpha '(95 95))
@@ -61,5 +64,6 @@
 ;; tomorrow-theme
 (load-file "~/.emacs.d/themes/tomorrow-theme/color-theme-tomorrow.el")
 ;; (require 'color-theme-tomorrow)
-(load-theme 'tomorrow-night t)
+(load-theme 'tomorrow-night-bright t)
+;; (load-theme 'tomorrow-night t)
 ;; (load-theme 'tomorrow-night tomorrow-night-eighties t)
